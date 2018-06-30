a338df900c85cbd82e0e330ead096635cc11a96c
Joao Antonio Chagas Nunes <joaoantoniocn@gmail.com>
2018-06-30 16:32:18 -0300

    Adicionado decaimento da taxa de aprendizagem no treinamento da rede

--- src/main.py
+++ src/main.py
@@ -24,13 +24,13 @@ parser.print("Base de dados carregada!")
 
 # --------------------- Cria RBF
 parser.print("Instanciando RBF...")
-taxa_aprendizagem = 0.01
-epocas = 1000
+taxa_aprendizagem = 0.001
+epocas = 2000
 rbf = RBF([len(set(labels)), len(set(labels))], taxa_aprendizagem, epocas)
+parser.print("taxa de aprendizzagem = " +str(taxa_aprendizagem))
+parser.print("numero de epocas = " +str(epocas))
 parser.print("RBF instanciada!")
 # -----
-
-# for aqui
 
 # ----- Separa base de dados em folders
 parser.print("Separando a base de dados em treino, teste e validação")
@@ -39,13 +39,19 @@ folders = cross.gerar_folders([])
 treino, teste, validacao, label_treino, label_teste, label_validacao = cross.separa_treino_teste(folders, 1, [2, 3])
 parser.print("Base de dados separada!")
 
+# for aqui
+
 # --- projetando base com eigenfaces
 parser.print("Projetando eigenfaces...")
 r = 1 # parametro fracionário
 representatividade = 0.9 # representatividade da projeção
 autova, autove, media_treino = parser.eigenfaces_fit(treino, r)
 treino_eig = parser.eigenfaces_transform_base(treino, autove, autova, media_treino, r, representatividade)
+parser.print("Base de treino projetada!")
 validacao_eig = parser.eigenfaces_transform_base(validacao, autove, autova, media_treino, r, representatividade)
+parser.print("Base de validacao projetada!")
+teste_eig = parser.eigenfaces_transform_base(teste, autove, autova, media_treino, r, representatividade)
+parser.print("Base de teste projetada!")
 parser.print("Dimensão da base de dados original:" + str(treino.shape))
 parser.print("Dimensão da base de dados projetada:" + str(treino_eig.shape))
 parser.print("Base de dados projetada!")
@@ -58,7 +64,7 @@ parser.print("Normalização da base de dados concluida! (treino, teste e valida
 
 # --- Treina rede
 parser.print("Iniciando treinamento da RBF...")
-rbf.fit(treino_eig, validacao_eig, parser.binariza2(label_treino), parser.binariza2(label_validacao))
+rbf.fit(treino_norm, validacao_norm, parser.binariza2(label_treino), parser.binariza2(label_validacao))
 parser.print("Treinamento finalizado!")
 # ---
 
--- src/rbf.py
+++ src/rbf.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
-from parser import Parser as parser
+from parser import Parser
 
 
 class RBF:
@@ -61,11 +61,17 @@ class RBF:
             for i in range(len(treino)):
                 self.erro = labels_treino[i] - self.predict(treino[i])
 
+                # se classificou errado atualiza os pesos
                 if( np.sum(self.erro) != 0):
                     self.atualiza_pesos()
 
+                # a cada 25% das epocas rodadas, diminui a taxa de aprendizagem
+                if((epocas%(self.epoca_treinamento/4)) == 0):
+                    self.taxa_aprendizagem = self.taxa_aprendizagem / 10
+                    Parser().print("Taxa de aprendizagem reduzida para :" + str(self.taxa_aprendizagem))
+
             if ((epocas%100) == 0):
-                parser.print("Epoca: " + str(epocas) + "   -   Acerto no treino: " + str(round(self.calcula_taxa_acerto(treino, labels_treino), 3)) + "   -   Acerto na validação: " + str(round(self.calcula_taxa_acerto(validacao, labels_validacao), 3)))
+                Parser().print("Epoca: " + str(epocas) + "   -   Acerto no treino: " + str(round(self.calcula_taxa_acerto(treino, labels_treino), 4)) + "   -   Acerto na validação: " + str(round(self.calcula_taxa_acerto(validacao, labels_validacao), 4)))
 
     def atualiza_pesos(self):
 
